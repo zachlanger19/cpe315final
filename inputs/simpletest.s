9e567e4d654f2a4e023c4e0db0294903d9bfad99
Jacob Rumpf <jrumpf@calpoly.edu>
2018-06-08 19:21:30 -0700

    still debugging

--- inputs/simpletest.s
+++ inputs/simpletest.s
@@ -11,8 +11,11 @@
 main:
     push {lr}
     sub	sp, sp, #16
-.L1
+    mov r1, #72
+    mov r2, #ab
+    mov r3, #56
+    mov r4, #8
+    push {}
     add	r7, sp, #0
-.L2
     pop {pc}
     .size	main, .-main
